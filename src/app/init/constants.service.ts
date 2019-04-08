29e50319d564faa7b887e4215b1047b75225e2a5
Harry Maxwell <enigcreator@gmail.com>
2019-04-09 00:59:42 +0500

    live url

--- src/app/init/constants.service.ts
+++ src/app/init/constants.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
-  public API_ENDPOINT = 'http://localhost:5000'
-  // public API_ENDPOINT = 'https://mongodb-multi-instance-test.herokuapp.com'
+  //public API_ENDPOINT = 'http://localhost:5000'
+  public API_ENDPOINT = 'https://mongodb-multi-instance-test.herokuapp.com'
   public Image_base_path = ''
 
   constructor() { }
