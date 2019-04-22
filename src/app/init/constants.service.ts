7bf79a71cce22cb5f0c36f83ac9186e3f13a974f
Luke Harvard <lukeharvardcp@gmail.com>
2019-04-23 02:43:59 +0500

    with live url

--- src/app/init/constants.service.ts
+++ src/app/init/constants.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
-  public API_ENDPOINT = 'http://localhost:5000'
-  // public API_ENDPOINT = 'https://mongodb-multi-instance-test.herokuapp.com'
+  // public API_ENDPOINT = 'http://localhost:5000'
+  public API_ENDPOINT = 'https://mongodb-multi-instance-test.herokuapp.com'
   public Image_base_path = '';
   public fbAppId = "668732456921248";
   public googleClientId = "287989781591-0l0m9f6dmgikf84c8223gbkfu8g27mus.apps.googleusercontent.com" //email used codingpixel.test6@gmail.com
