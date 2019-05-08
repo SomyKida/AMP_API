b4e494d6ef9b18efbfb93c306970a6bc8dadead8
Harry Maxwell <enigcreator@gmail.com>
2019-05-08 12:43:55 -0400

    added various mobile template images

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -8,10 +8,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TemplateComponent implements OnInit {
   @Input('primary') primary: string;
   @Input('secondary') secondary: string;
-
+  public current = 0;
+  public total_screens = 3;
   constructor() { }
 
   ngOnInit() {
   }
 
+  forward() {
+    if (this.current == this.total_screens)
+      this.current = 0;
+    else
+      this.current++;
+  }
+  backward() {
+    if (this.current == 0)
+      this.current = this.total_screens;
+    else
+      this.current--;
+  }
 }
