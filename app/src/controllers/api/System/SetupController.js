e72ca8062fa7a68d87ca55b8e340b9382fae07c6
Harry Maxwell <enigcreator@gmail.com>
2019-05-17 17:30:42 -0400

    landing page responsive

--- app/src/controllers/api/System/SetupController.js
+++ app/src/controllers/api/System/SetupController.js
@@ -4,6 +4,7 @@ const assert = require('assert');
 var express = require('express');
 var router = express.Router();
 var helper = require(helper_path + '/helper');
+var email = require(helper_path + '/email');
 var bcrypt = require('bcrypt');
 var fs = require('fs');
 
@@ -19,123 +20,128 @@ var ServiceRequest = require(base_path + '/app/src/models/ServiceRequest')
 var controller_name = 'setup';
 
 router.get('/json', (req, res) => {
-    if (req.query.msg == undefined) {
-        req.query.msg = ''
-    }
+  if (req.query.msg == undefined) {
+    req.query.msg = ''
+  }
 
-    helper.sendError(res, req.query.msg)
+  helper.sendError(res, req.query.msg)
 })
 router.get('/setup-db', (req, res) => {
 
-    Package.deleteMany({}, (err) => {
-        helper.postQueryDefault(err, res, 'Everything set up.')
-    })
+  Package.deleteMany({}, (err) => {
+    helper.postQueryDefault(err, res, 'Everything set up.')
+  })
 
 })
 router.get('/get-packages', (req, res) => {
 
-    Package.find({}, (err, result) => {
-        helper.postQueryDefault(err, res, result)
-    })
+  Package.find({}, (err, result) => {
+    helper.postQueryDefault(err, res, result)
+  })
 
 })
 router.post('/add-package', (req, res) => {
 
-    var post_data = req.body
+  var post_data = req.body
 
-    if (!helper.validateField(res, post_data, 'name', 'Name')) {
-        return
-    } if (!helper.validateField(res, post_data, 'price', 'Price')) {
-        return
-    }
+  if (!helper.validateField(res, post_data, 'name', 'Name')) {
+    return
+  } if (!helper.validateField(res, post_data, 'price', 'Price')) {
+    return
+  }
 
-    pack = new Package({
-        'name': post_data.name,
-        'price': post_data.price
-    })
+  pack = new Package({
+    'name': post_data.name,
+    'price': post_data.price
+  })
 
-    pack.save((err, result) => {
-        helper.postQueryDefault(err, res, 'Package inserted.')
-        return
-    })
+  pack.save((err, result) => {
+    helper.postQueryDefault(err, res, 'Package inserted.')
+    return
+  })
 
 })
 router.post('/add-theme', (req, res) => {
-    var post_data = req.body
+  var post_data = req.body
 
-    if (!helper.validateField(res, post_data, 'name', 'Name'))
-        return
+  if (!helper.validateField(res, post_data, 'name', 'Name'))
+    return
 
-    theme = new Theme({
-        'name': post_data.name,
-    })
+  theme = new Theme({
+    'name': post_data.name,
+  })
 
-    theme.save((err, result) => {
-        helper.postQueryDefault(err, res, 'Theme inserted.')
-        return
-    })
+  theme.save((err, result) => {
+    helper.postQueryDefault(err, res, 'Theme inserted.')
+    return
+  })
 })
 router.get('/get-themes', (req, res) => {
 
-    Theme.find({}, (err, result) => {
-        helper.postQueryDefault(err, res, result)
-    })
+  Theme.find({}, (err, result) => {
+    helper.postQueryDefault(err, res, result)
+  })
 
 })
 router.post('/add-service', (req, res) => {
-    var post_data = req.body
+  var post_data = req.body
 
-    if (!helper.validateField(res, post_data, 'name', 'Name'))
-        return
+  if (!helper.validateField(res, post_data, 'name', 'Name'))
+    return
 
-    service = new ServiceProvider({
-        'name': post_data.name,
-    })
+  service = new ServiceProvider({
+    'name': post_data.name,
+  })
 
-    service.save((err, result) => {
-        helper.postQueryDefault(err, res, 'Service Provider inserted.')
-        return
-    })
+  service.save((err, result) => {
+    helper.postQueryDefault(err, res, 'Service Provider inserted.')
+    return
+  })
 })
 router.get('/get-services', (req, res) => {
 
-    ServiceProvider.find({}, (err, result) => {
-        helper.postQueryDefault(err, res, result)
-    })
+  ServiceProvider.find({}, (err, result) => {
+    helper.postQueryDefault(err, res, result)
+  })
 
 })
 router.post('/add-service-request', (req, res) => {
-    var post_data = req.body
+  var post_data = req.body
 
-    if (!helper.validateField(res, post_data, 'name', 'Name'))
-        return
-    if (!helper.validateField(res, post_data, 'email', 'Email'))
-        return
+  if (!helper.validateField(res, post_data, 'name', 'Name'))
+    return
+  if (!helper.validateField(res, post_data, 'email', 'Email'))
+    return
 
-    service = new ServiceRequest({
-        'name': post_data.name,
-        'email': post_data.email,
+  service = new ServiceRequest({
+    'name': post_data.name,
+    'email': post_data.email,
+  })
+
+  service.save((err, result) => {
+    email.sendDefaultEmail('support@appmypractice.com', 'New Service Request', 'Service Name : ' + post_data.name + ' Email : ' + post_data.email, (err, info) => {
+      if (err) {
+        console.log(err)
+      }
     })
-
-    service.save((err, result) => {
-        helper.postQueryDefault(err, res, 'Feedback Saved.')
-        return
-    })
+    helper.postQueryDefault(err, res, 'Feedback Saved.')
+    return
+  })
 })
 router.post('/remove-dentist', (req, res) => {
-    var post_data = req.body
+  var post_data = req.body
 
-    if (!helper.validateField(res, post_data, 'email', 'Email'))
-        return
+  if (!helper.validateField(res, post_data, 'email', 'Email'))
+    return
 
-    Dentist.remove({ 'email': post_data.email }, (err) => {
-        if (err)
-            console.log(err)
-        else {
-            helper.sendError(res, "If email existed before, it doesn't now")
-            return
-        }
-    })
+  Dentist.remove({ 'email': post_data.email }, (err) => {
+    if (err)
+      console.log(err)
+    else {
+      helper.sendError(res, "If email existed before, it doesn't now")
+      return
+    }
+  })
 })
 
 module.exports = router
