d05c9426033c8acda448827f8526ff3b8c941606
Harry Maxwell <enigcreator@gmail.com>
2019-05-29 11:10:35 -0400

    fixed fonts on safari

--- src/app/init/constants.service.ts
+++ src/app/init/constants.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
-  //public API_ENDPOINT = 'http://localhost:5000'
-  public API_ENDPOINT = 'https://mongodb-multi-instance-test.herokuapp.com'
+  public API_ENDPOINT = 'http://localhost:5000'
+  //public API_ENDPOINT = 'https://mongodb-multi-instance-test.herokuapp.com'
   public Image_base_path = '';
   public fbAppId = "2376072312415673";
   public googleClientId = "424176729254-plsvkdgog2am23ad95i5et4agtrhbbca.apps.googleusercontent.com"
